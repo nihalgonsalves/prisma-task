39ecc3aa1b42ddb62b1dacb0ff98c3a5fd23be50
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-10-28 20:58:01 +0100

    feat: add parser

---
+++ src/parseQuery.spec.ts
@@ -0,0 +1,101 @@
+import { describe, expect, it } from "vitest";
+
+import { parseQuery, ParserError } from "./parseQuery";
+
+describe("parseQuery", () => {
+	it("parses a query with a single projection and a numeric > filter", () => {
+		const query = "PROJECT col1 FILTER col1 > 1";
+
+		expect(parseQuery(query)).toEqual({
+			projections: ["col1"],
+			filter: {
+				column: "col1",
+				operator: ">",
+				value: 1,
+			},
+		});
+	});
+
+	it("parses a query with a multiple projections and a numeric > filter", () => {
+		const query = "PROJECT col1, col2 FILTER col1 > 1";
+
+		expect(parseQuery(query)).toEqual({
+			projections: ["col1", "col2"],
+			filter: {
+				column: "col1",
+				operator: ">",
+				value: 1,
+			},
+		});
+	});
+
+	it("parses a query with a multiple projections and a numeric = filter", () => {
+		const query = "PROJECT col1, col2 FILTER col1 = 1";
+
+		expect(parseQuery(query)).toEqual({
+			projections: ["col1", "col2"],
+			filter: {
+				column: "col1",
+				operator: "=",
+				value: 1,
+			},
+		});
+	});
+
+	it("parses a query with a multiple projections and a string = filter", () => {
+		const query = 'PROJECT col1, col2 FILTER col1 = "hello"';
+
+		expect(parseQuery(query)).toEqual({
+			projections: ["col1", "col2"],
+			filter: {
+				column: "col1",
+				operator: "=",
+				value: "hello",
+			},
+		});
+	});
+
+	it("parses a query with no spacing within projections and the filter", () => {
+		const query = 'PROJECT col1,col2 FILTER col1="hello"';
+
+		expect(parseQuery(query)).toEqual({
+			projections: ["col1", "col2"],
+			filter: {
+				column: "col1",
+				operator: "=",
+				value: "hello",
+			},
+		});
+	});
+
+	it("parses a query with differently cased keywords", () => {
+		const query = 'PROject col1, col2 filtER col1 = "hello"';
+
+		expect(parseQuery(query)).toEqual({
+			projections: ["col1", "col2"],
+			filter: {
+				column: "col1",
+				operator: "=",
+				value: "hello",
+			},
+		});
+	});
+
+	it("throws a ParserError if the query is incomplete", () => {
+		const query = "PROJECT col1,col2 FILTER";
+
+		expect(() => parseQuery(query)).toThrowError(ParserError);
+	});
+
+	it("throws a ParserError if there's a > operand with a string value", () => {
+		const query = 'PROJECT col1, col2 FILTER col1 > "hello"';
+
+		expect(() => parseQuery(query)).toThrowError(ParserError);
+	});
+
+	it("throws a ParserError if the column names are not alphanumeric literals", () => {
+		const query = 'PROJECT col$1, col2 FILTER col1 = "hello"';
+
+		expect(() => parseQuery(query)).toThrowError(ParserError);
+	});
+});
---
+++ src/parseQuery.ts
@@ -0,0 +1,102 @@
+import { z } from "zod";
+
+type OperatorGreaterThan = ">";
+type OperatorEqual = "=";
+
+type Filter = { column: string } & (
+	| {
+			operator: OperatorGreaterThan;
+			value: number;
+	  }
+	| {
+			operator: OperatorEqual;
+			value: number | string;
+	  }
+);
+
+type Query = {
+	projections: string[];
+	filter: Filter;
+};
+
+export class ParserError extends Error {}
+
+// Since the syntax is a single line, use a simple regex instead of a full
+// parser. The downsides are that error messages are more vague and the grammar
+// is much less extensible. I chose this tradeoff as this would still support
+// adding multiple filters, but not much else.
+
+// A middleground for error reporting could be to loosen the matchers in the
+// regex, keeping only the structure, and validating in the ZodSchema, giving
+// more granular errors
+const QueryRegEx =
+	/^PROJECT\s+(?<projections>[A-Za-z0-9\s,]*)\s+FILTER\s+(?<filterColumn>[A-Za-z0-9]*)\s*(?<filterOperand>>|=)\s*(?<filterValue>[0-9]+|"\w*")$/i;
+
+const CommaSeparatedProjections = z
+	.string()
+	.transform((v) => v.split(",").map((p) => p.trim()));
+
+const QueryMatchGroupSchema = z.discriminatedUnion("filterOperand", [
+	z.object({
+		projections: CommaSeparatedProjections,
+		filterColumn: z.string(),
+		filterOperand: z.literal(">"),
+		filterValue: z.coerce.number().int(),
+	}),
+	z.object({
+		projections: CommaSeparatedProjections,
+		filterColumn: z.string(),
+		filterOperand: z.literal("="),
+		filterValue: z.union([
+			z.coerce.number().int(),
+			z
+				.string()
+				.regex(/"\w*"/)
+				.transform((v) => v.slice(1, -1)),
+		]),
+	}),
+]);
+
+/**
+ * A narrowly-defined syntax:
+ * - single line
+ * - projections are comma-separated
+ * - columns are alphanumeric
+ * - only one filter is allowed - LHS is the column, RHS is a value
+ * - values are either number literals or strings enclosed in double quotes
+ *
+ * e.g.: PROJECT col1, col2 FILTER col3 > "value"
+ */
+export const parseQuery = (query: string): Query => {
+	const match = QueryRegEx.exec(query);
+	if (!match?.groups) {
+		throw new ParserError("Invalid query");
+	}
+
+	const parsedGroups = QueryMatchGroupSchema.safeParse(match.groups);
+	if (!parsedGroups.success) {
+		throw new ParserError(parsedGroups.error.message);
+	}
+
+	// required to satisfy TypeScript's discriminated union check
+
+	if (parsedGroups.data.filterOperand === ">") {
+		return {
+			projections: parsedGroups.data.projections,
+			filter: {
+				column: parsedGroups.data.filterColumn,
+				operator: parsedGroups.data.filterOperand,
+				value: parsedGroups.data.filterValue,
+			},
+		};
+	}
+
+	return {
+		projections: parsedGroups.data.projections,
+		filter: {
+			column: parsedGroups.data.filterColumn,
+			operator: parsedGroups.data.filterOperand,
+			value: parsedGroups.data.filterValue,
+		},
+	};
+};
