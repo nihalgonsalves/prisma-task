7aa85740562087cde3ac8e83966ab425e95fdfde
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-10-29 16:22:43 +0100

    chore: generate more realistic fake data

--- bin/generate.ts
+++ bin/generate.ts
@@ -3,17 +3,38 @@ import papa from "papaparse";
 
 import { range, getFileWriteStream } from "../src/util";
 
-const COL_COUNT = 10;
 const ROW_COUNT = 10_000;
 const OUTPUT_FILE = new URL("../data/data.csv", import.meta.url);
 
-type ColumnType = "string" | "integer";
+let id = 0;
 
 // Generate columns and pre-specify each type
-const columns = Array.from({ length: COL_COUNT }, (_, i) => ({
-	name: `col${i + 1}`,
-	type: faker.helpers.arrayElement<ColumnType>(["string", "integer"]),
-}));
+const columns = [
+	{
+		name: "id",
+		generate: () => id++,
+	},
+	{
+		name: "name",
+		generate: () => faker.person.firstName(),
+	},
+	{
+		name: "email",
+		generate: () => faker.internet.email(),
+	},
+	{
+		name: "age",
+		generate: () => faker.number.int({ min: 0, max: 100 }),
+	},
+	{
+		name: "city",
+		generate: () => faker.location.city(),
+	},
+	{
+		name: "favourite_fruit",
+		generate: () => faker.food.fruit(),
+	},
+];
 
 using output = getFileWriteStream(OUTPUT_FILE, {
 	encoding: "utf8",
@@ -25,15 +46,7 @@ output.writeLn(papa.unparse([columns.map(({ name }) => name)]));
 
 // eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars
 for (const _ of range(ROW_COUNT)) {
-	output.writeLn(
-		papa.unparse([
-			columns.map(({ type }) =>
-				type === "string"
-					? faker.word.noun()
-					: faker.number.int({ min: 0, max: 10_000 }).toFixed(0),
-			),
-		]),
-	);
+	output.writeLn(papa.unparse([columns.map(({ generate }) => generate())]));
 }
 
 console.log("Done");
